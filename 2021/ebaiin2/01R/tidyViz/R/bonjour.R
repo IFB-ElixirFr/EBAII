09018a59108a484dd1f3a9f0c4f7a928fc14ef0f
Vincent  GUILLEMOT <vincent.guillemot@pasteur.fr>
2021-05-23 09:48:35 +0200

    UPDATE ggplot2 slides

--- 2021/ebaiin2/01R/tidyViz/R/bonjour.R
+++ 2021/ebaiin2/01R/tidyViz/R/bonjour.R
@@ -2,6 +2,8 @@
 #' 
 #' "Bonjour !" : Cette fonction est un exemple de fonction graphique avec deux paramètres simples : la couleur du texte et la couleur du fond.
 #' 
+#' PS : cette fonction ne servira jamais pour le cours, elle sert juste à dire bonjour !
+#' 
 #' @param couleur la couleur du texte
 #' @param fond la couleur du fond
 #'
@@ -19,8 +21,30 @@ bonjour <- function(couleur = "pink", fond = "limegreen") {
 
 #' Le jeu de données fruits
 #' 
-#' Composition nutritionnelle de 57 composés alimentaires issus du groupe "fruits" défini dans les données Ciqual de 2020.
+#' Composition nutritionnelle de 51 composés alimentaires issus du groupe "fruits" défini dans les données Ciqual de 2020.
 #'
+#' La table fruits est de classe "tibble" et contient les colonnes suivantes : 
+#'\itemize{
+#'  \item nom : Nom du fruit
+#'  \item groupe : Groupe de fruit
+#'  \item Energie : Energie, (kJ/100 g)
+#'  \item Eau : Eau (g/100 g)
+#'  \item Proteines : Protéines, N x 6.25 (g/100 g)
+#'  \item Glucides : Glucides (g/100 g)
+#'  \item Lipides : Lipides (g/100 g)
+#'  \item Sucres : Sucres (g/100 g)
+#'  \item Fructose : Fructose (g/100 g)
+#'  \item Fibres : Fibres alimentaires (g/100 g)
+#'  \item Calcium : Calcium (mg/100 g)
+#'  \item Magnesium : Magnésium (mg/100 g)
+#'  \item Phosphore : Phosphore (mg/100 g)
+#'  \item Potassium : Potassium (mg/100 g)
+#'  \item Zinc : Zinc (mg/100 g)
+#'  \item BetaCarotene : Beta-Carotène (µg/100 g)
+#'  \item VitamineE : Vitamine E (mg/100 g)
+#'  \item VitamineC : Vitamine C (mg/100 g)
+#'}
+#'  
 #' @name fruits
 #' @docType data
 #' @references \url{https://ciqual.anses.fr/}
