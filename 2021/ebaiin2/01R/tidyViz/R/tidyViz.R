fc1948e0cd9c63cadc6a58d36b5915266a597433
lkhamvongsa <lucie.khamvongsa@gmail.com>
2025-10-24 10:09:05 +0200

    content: include 2020 N1 training resources

--- 2021/ebaiin2/01R/tidyViz/R/tidyViz.R
+++
@@ -1,4 +0,0 @@
-#' Package pour la formation EBAII niveau 2
-#' Contient les vignettes (les slides de cours), le jeu de données "fruits" et les tutoriels. https://ifb-elixirfr.github.io/EBAII/2021/ebaiin2/ 
-#' @keywords internal
-"_PACKAGE"
